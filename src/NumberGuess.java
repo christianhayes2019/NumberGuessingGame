e7d16c07d0d9e39f6eca079d44739564fbbbc84e
Unknown <christian.hayes.2019@student.lhschools.org>
2018-09-18 14:12:02 -0400

    Coded Number Guessing Game

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class NumberGuess {
     public static void main(String [] args) {
-        
+
+        int pickedNumber;
+        Scanner keyboard;
+        int userGuess;
+        boolean wonGame =false;
+        pickedNumber = (int) (Math.random() * 100);
+        keyboard = new Scanner(System.in);
+
+
+        System.out.println(pickedNumber);
+
+        while (!wonGame) {
+            System.out.println("The system is thinking of a number.");
+            System.out.println("Guess a number from 0-100.");
+            userGuess = keyboard.nextInt();
+            if (userGuess > pickedNumber) {
+                System.out.print("You're too high!");
+            } else if (userGuess < pickedNumber) {
+                System.out.println("You're too low!");
+            }
+            if (userGuess == pickedNumber) {
+                System.out.println("CONGRATS!");
+                System.out.print("The number is " + pickedNumber +"!");
+
+                //wonGame = true;
+                break;
+            }
+
+        }
+        System.out.println(" Thanks for playing!");
     }
 }
+
+
