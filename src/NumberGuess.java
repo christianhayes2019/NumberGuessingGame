7c8facedd4e196dffab0e399f268ec2b683c6149
Unknown <christian.hayes.2019@student.lhschools.org>
2018-09-20 14:16:41 -0400

    Added scoreboard and limited number of guesses

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -6,32 +6,37 @@ public class NumberGuess {
         int pickedNumber;
         Scanner keyboard;
         int userGuess;
+        int numberOfTries = 0;
         boolean wonGame =false;
+        boolean play =true;
         pickedNumber = (int) (Math.random() * 100);
         keyboard = new Scanner(System.in);
 
 
-        System.out.println(pickedNumber);
 
-        while (!wonGame) {
+
             System.out.println("The system is thinking of a number.");
             System.out.println("Guess a number from 0-100.");
-            userGuess = keyboard.nextInt();
-            if (userGuess > pickedNumber) {
-                System.out.print("You're too high!");
-            } else if (userGuess < pickedNumber) {
-                System.out.println("You're too low!");
-            }
-            if (userGuess == pickedNumber) {
-                System.out.println("CONGRATS!");
-                System.out.print("The number is " + pickedNumber +"!");
+            while (!wonGame && numberOfTries < 10) {
+                userGuess = keyboard.nextInt();
+                numberOfTries++;
 
-                //wonGame = true;
-                break;
+                if (userGuess > pickedNumber) {
+                    System.out.print("You're too high!");
+                } else if (userGuess < pickedNumber) {
+                    System.out.println("You're too low!");
+                }
+                if (userGuess == pickedNumber) {
+                    System.out.println("CONGRATS YOU WIN!");
+                    System.out.println("It took you " + numberOfTries + " tries to guess the number " + pickedNumber + "!");
+                    System.out.println("Thanks for playing!");
+                }
+                if (numberOfTries == 10) {
+                    System.out.println("YOU LOSE!");
+                    System.out.print("Please try again.");
+                }
             }
 
-        }
-        System.out.println(" Thanks for playing!");
     }
 }
 
